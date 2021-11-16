61c382e329ad68ecb3ea0214e4b369d54ffb68bc
Paul Molczanski <paulmski@gmail.com>
2021-11-16 09:47:13 -0500

    Created Main.java which is where the program will execute code.

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Paul Molczanski
+ * @email paulmski@gmail.com
+ * @create date November 16, 2021 09:44
+ * @modify date November 16, 2021 09:44
+ * @desc The main class which will execute.
+ */
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
--- Ship.java
+++ Ship.java
@@ -3,5 +3,35 @@
  * @email paulmski@gmail.com
  * @create date November 16, 2021 09:37
  * @modify date November 16, 2021 09:37
- * @desc This class is the basic model for all ships. It contains it's appearance along with name and other information.
+ * @desc This class is the basic model for all ships. It contains its appearance along with name and other information.
  */
+
+
+ public class Ship {
+     // The name of the ship
+     private String name;
+     // The appearance of it on the board
+     private String appearance;
+
+
+
+
+
+     // ********** Setter and getter methods ********
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAppearance() {
+        return this.appearance;
+    }
+
+    public void setAppearance(String appearance) {
+        this.appearance = appearance;
+    }
+     
+ }
