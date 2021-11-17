a3f4d8bf1b51a7fa70f310d7ad06e5ccdeef11f4
paulmski <paulmski@gmail.com>
2021-11-17 13:19:36 -0500

    Completed getPixel method for Input class

--- src/Input.java
+++ src/Input.java
@@ -1,3 +1,4 @@
+
 /**
  * @author Paul Molczanski
  * @email paulmski@gmail.com
@@ -8,30 +9,51 @@
 
 import java.util.Scanner;
 
-
 public class Input {
-    
 
     public static Pixel getPixel(String message, int lowerRow, int upperRow, int lowerCol, int upperCol) {
         // Pixel to be eventually returned
-        Pixel pixel = Pixel();
-
-
+        Pixel pixel = new Pixel();
 
         System.out.println(message);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Select row between: " + (char)lowerRow+65 + " and " + (char)upperRow+65);
+        System.out.println("Select row between: " + (char) lowerRow + 65 + " and " + (char) upperRow + 65);
         String input = scanner.next();
-        // The given input must be of length one otherwise the input will be asked for again.
+        // The given input must be of length one otherwise the input will be asked for
+        // again.
         if (input.length() != 1) {
             System.out.println("Sorry, invalid input please try again.");
             return getPixel(message, lowerRow, upperRow, lowerCol, upperCol);
         }
-        // Indexing of string is safe because it is known that the string is exactly of length 1
-        int row = (int) input.charAt(0);
-        pixel
+        // Indexing of string is safe because it is known that the string is exactly of
+        // length 1
+        int row = ((int) input.charAt(0)) - 65;
+        if (row >= lowerRow && row <= upperRow) {
+            pixel.setRow(row);
+        } else {
+            System.out.println("Sorry, invalid input please try again.");
+            return getPixel(message, lowerRow, upperRow, lowerCol, upperCol);
+        }
 
-        
+        System.out.println("Select column between: " + lowerCol + " and " + upperCol);
+        int input2 = scanner.nextInt();
+
+        // Indexing of string is safe because it is known that the string is exactly of
+        // length 1
+        if (input2 >= lowerRow && input2 <= upperRow) {
+            pixel.setRow(input2);
+        } else {
+            System.out.println("Sorry, invalid input please try again.");
+            return getPixel(message, lowerRow, upperRow, lowerCol, upperCol);
+        }
+
+        // Prevent memory leak.
+        scanner.close();
+        return pixel;
     }
+
+
+
+    
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
  * @author Paul Molczanski
  * @email paulmski@gmail.com
  * @create date November 16, 2021 09:44
- * @modify date November 16, 2021 09:44
+ * @modify date 2021-11-17 13:12:25
  * @desc The main class which will execute.
  */
 
@@ -10,18 +10,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board(20,20, '#');
-      for (int i = 0; i <3000; i++) {
-          try {
-            board.setObject(String.valueOf(i), 10, 10, 1);
-            board.drawBoard();
-            Thread.sleep(33);
-            Board.drawBlank();
-
-          
-          } catch (InterruptedException e) {
-
-          }
-        }
+      
     }
 }
