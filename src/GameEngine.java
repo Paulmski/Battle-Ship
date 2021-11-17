02d92300b961f109f60b05199a36269dbe97c48e
Paul Molczanski <paulmski@gmail.com>
2021-11-17 17:24:33 -0500

    Added gamemode mechanic and processes user input.

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -10,8 +10,11 @@
 
 
 public class GameEngine {
-    
+    // A bool to tell the Main Class if it should continue drawing frames.
     private boolean running = true;
+    // 0: Single Player mode
+    // 1: Two player mode
+    private int gameMode = 0;
 
     
 
@@ -31,6 +34,20 @@ public class GameEngine {
         mainBoard.setObject("3:  Quit Game",12,10,1);        
         mainBoard.drawBoard();
         int input  = Input.getInt("Enter 1, 2, or 3: ", 1, 3);
+        switch (input) {
+            // Selected single player mode
+            case 1:
+                gameMode = 0;
+                break;
+            // Selected two player mode
+            case 2:
+                gameMode = 1;
+                break;
+            // Quit the game
+            case 3:
+                this.setRunning(false);
+                return;
+        }
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -55,6 +72,8 @@ public class GameEngine {
     }
 
 
+
+    // ********** Class specific methods ***********
 
     
 }
