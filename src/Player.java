4034cc7bc75197211400b20fb529d3ee6e958816
Paul Molczanski <paulmski@gmail.com>
2021-11-17 09:39:31 -0500

    Created player class

---
+++ src/Player.java
@@ -0,0 +1,20 @@
+/**
+ * @author Paul Molczanski
+ * @email paulmski@gmail.com
+ * @create date 2021-11-17 09:36:58
+ * @modify date 2021-11-17 09:36:58
+ * @desc The class which will contain player information like guesses, battleship placement, and scoring.
+ */
+
+
+
+public class Player {
+    
+    private char[][] guesses;
+    private char[][] shipPlacements;
+    private String name;
+
+
+
+
+}
